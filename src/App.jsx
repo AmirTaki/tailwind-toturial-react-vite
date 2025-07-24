b4688639a21bbd8670d519ef0ebe79a5cd7ca917
amir <amirtaki623@gmail.com>
2025-07-24 16:43:24 +0330

    Compatibility : typography & refrence @apply

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import './App.css'
+import './App.css';
+
+
+
 
 function App() {
   return (
     <>
-      <h1 className = "text-3xl font-bold underline text-center ">
-        Hello World!
-      </h1>
+
+
+
+    <div className='typography'>
+      <h1>test typography</h1>
+      <p>pragraph typography</p>
+      <img src="https://tse1.mm.bing.net/th/id/OIP._V_aeNtpiKzdt7EkrSzh1wEsEs?pid=Api&P=0&h=220" alt="" />
+    </div>
+    <button class = "bg-indigo-200 hover:bg-indigo-950">
+      button Color
+    </button>
+    
+    <button>
+      refrecne click
+    </button>
     </>
+
   )
 }
 
 export default App
+
