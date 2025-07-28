eb0934b5bc6808cef1ea787f75f4b669f0002c7b
amir <amirtaki623@gmail.com>
2025-07-29 02:24:26 +0330

    Dark Mode System

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import './App.css';
 
 
 function App() {
-
-  const [theme, setTheme] =  useState(true)
+    document.documentElement.classList.toggle(
+      "dark",
+      localStorage.theme === "dark" ||
+        (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
+    );
+  const [theme, setTheme] =  useState(localStorage.theme)
 
   return (
     <>    
