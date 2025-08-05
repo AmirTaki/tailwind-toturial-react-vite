038369d7074e8339ca0a2e5ae1956607ee4fd158
amir <amirtaki623@gmail.com>
2025-08-05 04:01:04 +0330

    Functions and directives

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,66 @@
 import './App.css';
-import Button from './component/Button';
-import ButtonTwo from './component/ButtonTwo';
 
 function App() {
-  const error = true;
+
   return (
     
     <>    
-      <h1 className='bg-blue-900  text-white my-4 text-center'>Detecting classes in source files</h1>
-      <h1 className='bg-blue-900 text-white my-4 text-center'>    How classes are detected</h1>
+      <h1 className='bg-blue-900  text-white my-4 text-center'>Functions and directives</h1>
+      <h1 className='bg-blue-900  text-white my-4 text-center'>@ import</h1>
+
+      <div className="bg-orange-400 p-5 text-green-900 hover:scale-90">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias fugit dolore reiciendis magnam? Nihil possimus aspernatur necessitatibus eligendi itaque ipsum rem vel non? Aut harum consequuntur accusantium? Velit, optio blanditiis!
+      </div>
+      <h1 className='bg-blue-900  text-white my-4 text-center'>@ theme</h1>
+
+      <div className="font-display text-avocado-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, officia quibusdam quasi placeat minima vel aliquam asperiores? Est magnam excepturi nobis, pariatur dolor culpa, quia perspiciatis assumenda laboriosam facere numquam!</div>
+
+      <h1 className='bg-blue-900  text-white my-4 text-center'>@ source</h1>
       
-      <Button color = "white" children="click button"/>
-      <Button color = "black" children="click button"/>
-      <Button color = "blue" children="click button"/>
-
-      <h1 className='bg-blue-900 text-white my-4 text-center'>  Always use complete class names</h1>    
-
-      <div className= {error ? "text-green-500" : "text-red-500"}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores accusamus optio tempore molestias nemo delectus nam ratione corporis eum eligendi natus excepturi provident minus, facilis suscipit ullam consectetur maiores atque.
+      <h1 className='bg-blue-900  text-white my-4 text-center'>@ utility</h1>
+      <div className="bg-red-400 p-3">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, vel recusandae officiis numquam tempora odit dolores vero quaerat doloribus neque facere laudantium veniam illum accusantium, harum omnis laboriosam deleniti! Sapiente.
       </div>
 
-      <h1 className='bg-blue-900 text-white my-4 text-center'>  Always map props to static class names</h1>    
-      <ButtonTwo color = "red" children = "clickMe"/>
-      <ButtonTwo color = "blue" children = "clickMe"/>
-      <ButtonTwo color = "white" children = "clickMe"/>
+      <h1 className='bg-blue-900  text-white my-4 text-center'>@ variant </h1>
+      <div data-theme = "">
+        <div className="my-element" >
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore cumque ipsum aperiam quod eos accusamus commodi magni aliquid nesciunt, est, aspernatur qui quidem unde praesentium quis voluptates beatae similique nemo.
+        </div>
+      </div>
+
+      <h1 className='bg-blue-900  text-white my-4 text-center'>@ custom-variant </h1>
+      <div data-theme = "midnight">
+        <div className="alpha:m-3" >
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore cumque ipsum aperiam quod eos accusamus commodi magni aliquid nesciunt, est, aspernatur qui quidem unde praesentium quis voluptates beatae similique nemo.
+        </div>
+      </div>
+
+      <h1 className='bg-blue-900  text-white my-4 text-center'>@  apply </h1>
+
+      <div className="select2-dropdown" >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore cumque ipsum aperiam quod eos accusamus commodi magni aliquid nesciunt, est, aspernatur qui quidem unde praesentium quis voluptates beatae similique nemo.
+      </div>
+      
+      <h1 className='bg-blue-900  text-white my-4 text-center'>@  reference --->> viu.js </h1>
+   
+      <h1 className='bg-blue-900  text-white my-4 text-center'>--alpha()</h1>
+
+      <div className="bg-avocado-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat error obcaecati nihil saepe pariatur excepturi quasi sed. Unde, consequatur repellendus? Officiis aspernatur assumenda ut quae aliquid excepturi sint distinctio id.</div>
+      <div className="ClassAlpha">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat error obcaecati nihil saepe pariatur excepturi quasi sed. Unde, consequatur repellendus? Officiis aspernatur assumenda ut quae aliquid excepturi sint distinctio id.</div>
+    
+      <h1 className='bg-blue-900  text-white my-4 text-center'>--spacing()</h1>
+
+      <div className="SpacingAlpha bg-avocado-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, magnam. Aspernatur quidem modi aut omnis. Nihil deleniti repudiandae, iste temporibus repellendus perspiciatis id. Nemo earum voluptatibus perspiciatis odio quod iusto.</div>
+    
+      <div className="bg-red-400 py-[calc(--spacing(6)-2px)]">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla ea atque recusandae, libero animi in nobis tempore tempora fugiat vitae nesciunt iste, at illo iure laboriosam reprehenderit consectetur placeat delectus!
+      </div>
+      <h1 className='bg-blue-900  text-white my-4 text-center'>theme()</h1>
+
+      <div className="theme bg-amber-300">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium nihil officiis hic numquam porro sunt inventore debitis, explicabo, illo exercitationem eos officia ut aliquid alias in nobis culpa, error sapiente?
+      </div>
     </>
 
 
