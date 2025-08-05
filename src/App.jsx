36d9fd0eb6822ca678e72c84de39c02cff8661f0
amir <amirtaki623@gmail.com>
2025-08-05 21:30:23 +0330

    aspect-ratio

--- src/App.jsx
+++ src/App.jsx
@@ -5,62 +5,30 @@ function App() {
   return (
     
     <>    
-      <h1 className='bg-blue-900  text-white my-4 text-center'>Functions and directives</h1>
-      <h1 className='bg-blue-900  text-white my-4 text-center'>@ import</h1>
-
-      <div className="bg-orange-400 p-5 text-green-900 hover:scale-90">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias fugit dolore reiciendis magnam? Nihil possimus aspernatur necessitatibus eligendi itaque ipsum rem vel non? Aut harum consequuntur accusantium? Velit, optio blanditiis!
-      </div>
-      <h1 className='bg-blue-900  text-white my-4 text-center'>@ theme</h1>
-
-      <div className="font-display text-avocado-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, officia quibusdam quasi placeat minima vel aliquam asperiores? Est magnam excepturi nobis, pariatur dolor culpa, quia perspiciatis assumenda laboriosam facere numquam!</div>
-
-      <h1 className='bg-blue-900  text-white my-4 text-center'>@ source</h1>
-      
-      <h1 className='bg-blue-900  text-white my-4 text-center'>@ utility</h1>
-      <div className="bg-red-400 p-3">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, vel recusandae officiis numquam tempora odit dolores vero quaerat doloribus neque facere laudantium veniam illum accusantium, harum omnis laboriosam deleniti! Sapiente.
-      </div>
-
-      <h1 className='bg-blue-900  text-white my-4 text-center'>@ variant </h1>
-      <div data-theme = "">
-        <div className="my-element" >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore cumque ipsum aperiam quod eos accusamus commodi magni aliquid nesciunt, est, aspernatur qui quidem unde praesentium quis voluptates beatae similique nemo.
-        </div>
-      </div>
-
-      <h1 className='bg-blue-900  text-white my-4 text-center'>@ custom-variant </h1>
-      <div data-theme = "midnight">
-        <div className="alpha:m-3" >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore cumque ipsum aperiam quod eos accusamus commodi magni aliquid nesciunt, est, aspernatur qui quidem unde praesentium quis voluptates beatae similique nemo.
-        </div>
-      </div>
-
-      <h1 className='bg-blue-900  text-white my-4 text-center'>@  apply </h1>
-
-      <div className="select2-dropdown" >
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore cumque ipsum aperiam quod eos accusamus commodi magni aliquid nesciunt, est, aspernatur qui quidem unde praesentium quis voluptates beatae similique nemo.
-      </div>
-      
-      <h1 className='bg-blue-900  text-white my-4 text-center'>@  reference --->> viu.js </h1>
+      <h1 className='bg-blue-900  text-white my-4 text-center'>aspect-ratio</h1>
    
-      <h1 className='bg-blue-900  text-white my-4 text-center'>--alpha()</h1>
+      <h1 className='bg-blue-900  text-white my-4 text-center'>Basic example</h1>
 
-      <div className="bg-avocado-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat error obcaecati nihil saepe pariatur excepturi quasi sed. Unde, consequatur repellendus? Officiis aspernatur assumenda ut quae aliquid excepturi sint distinctio id.</div>
-      <div className="ClassAlpha">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat error obcaecati nihil saepe pariatur excepturi quasi sed. Unde, consequatur repellendus? Officiis aspernatur assumenda ut quae aliquid excepturi sint distinctio id.</div>
-    
-      <h1 className='bg-blue-900  text-white my-4 text-center'>--spacing()</h1>
+      <img src="https://tse4.mm.bing.net/th/id/OIP.HxV79tFMPfBAIo0BBF-sOgHaEy?pid=Api&P=0&h=220" className='aspect-3/2 object-cover' alt="" />
 
-      <div className="SpacingAlpha bg-avocado-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, magnam. Aspernatur quidem modi aut omnis. Nihil deleniti repudiandae, iste temporibus repellendus perspiciatis id. Nemo earum voluptatibus perspiciatis odio quod iusto.</div>
-    
-      <div className="bg-red-400 py-[calc(--spacing(6)-2px)]">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla ea atque recusandae, libero animi in nobis tempore tempora fugiat vitae nesciunt iste, at illo iure laboriosam reprehenderit consectetur placeat delectus!
-      </div>
-      <h1 className='bg-blue-900  text-white my-4 text-center'>theme()</h1>
+      <h1 className='bg-blue-900  text-white my-4 text-center'>Using a video aspect ratio</h1>
 
-      <div className="theme bg-amber-300">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium nihil officiis hic numquam porro sunt inventore debitis, explicabo, illo exercitationem eos officia ut aliquid alias in nobis culpa, error sapiente?
-      </div>
+      <iframe class="aspect-video " src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>
+
+      <h1 className='bg-blue-900  text-white my-4 text-center'> Using a custom value</h1>
+
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9EZNefY1fRsA4qVFTBviWyj-5KHY6U8LG0g&s" className='aspect-[calc(4*3+1)/3] ' alt="" />
+
+      <br />
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9EZNefY1fRsA4qVFTBviWyj-5KHY6U8LG0g&s" className='aspect-(--my-aspect-ratio) ' alt="" />
+
+      <h1 className='bg-blue-900  text-white my-4 text-center'>Responsive design</h1>
+
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2m5lrUnvYTjtpAQlMry8arqTYivCPiRYfkA&s" className='aspect-video md:aspect-square' alt="" />
+
+      <h1 className='bg-blue-900  text-white my-4 text-center'>Customizing your theme</h1>
+
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCZlf5lc5tX-0gY-y94pGS0mQdL-D0lCH2OQ&s" className='aspect-retro' alt="" />
     </>
 
 
