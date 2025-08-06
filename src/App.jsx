50417b216fbd8d311948fe816e134e1d0791a3fe
amir <amirtaki623@gmail.com>
2025-08-07 03:09:25 +0330

    box-sizing

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,18 @@ function App() {
     
     <>    
       <h1 className='bg-blue-900  text-white my-4 text-center'>box-decoration-break</h1>
-
-
-      <div className="" >
-        <h2 style={{    backgroundImage: "linear-gradient(to right, #ffef7e, #b7f9e9)", width:'250px' , fontSize:"30px", display:"inline", padding:"5px" ,border:"2px solid gray" , boxDecorationBreak:"clone", WebkitBoxDecorationBreak:"clone" }}>
-          Box Decoration Break Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti maiores exercitationem nam dicta non cumque sit qui in vitae veniam quae eum quam mollitia, sunt enim porro quos vero natus!
-        </h2>
-
-        <br />
-        <br />
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi aliquam animi est eveniet in? Sequi voluptates nobis iusto, suscipit nam voluptate corporis quos ducimus possimus rem eveniet nostrum laborum officiis!</p>
-      </div>
-
-      <br />
-      <br />
-      <br />
-      <h1 className='bg-blue-900  text-white my-4 text-center'>Basic example</h1>
-      <div className="bg-black">
-        <span className='box-decoration-slice bg-linear-to-r from-indigo-600 to-pink-500 px-2 text-white font-bold'>
-          Hello<br />World
-        </span>
-        <br />
-      </div>
-      <br />
-      <div className="bg-black">
-        <span className='box-decoration-clone bg-linear-to-r from-indigo-600 to-pink-500 px-2 text-white font-bold'>
-          Hello<br />World
-        </span>
-        <br />
-      </div>
-
+      <div className="flex items-center justify-center flex-col">
+        <div className="w-70 h-50 bg-amber-500  border-10 border-e-red-600 box-border">
+          box-sizing : border-box
+        </div>
+        <div className="w-70 h-50 bg-gray-500 "></div>
+        <div className="w-70 h-50 bg-green-500 border-10 border-e-red-600 box-content">
+          box-sizing : content-box
+        </div>
+      
+      </div>     
+     
+      
 
     </>
 
