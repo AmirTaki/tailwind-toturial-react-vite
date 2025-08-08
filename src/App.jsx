cad872bf4470ad6ef821c05e13bce066df02b813
amir <amirtaki623@gmail.com>
2025-08-09 03:00:08 +0330

    flex-direction

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,35 @@ function App() {
   return (
     
     <>    
-      <h1 className='bg-blue-900  text-white my-4 text-center'>flex-basis</h1>
-      <h1 className='bg-blue-900  text-white my-4 text-center'>Using the spacing scale</h1>
-      <div className="flex items-center justify-around">
-        <div className="basis-20 w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-pink-400">01</div>
-        <div className="basis-50 w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-pink-400">02</div>
-        <div className="basis-150  w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-pink-400">03</div>
+      <h1 className='bg-blue-900  text-white my-4 text-center'>flex-direction</h1>
+      <h1 className='bg-blue-900  text-white my-4 text-center'>Row</h1>
+      <div className="flex flex-row  ">
+        <div className=" w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-pink-400">01</div>
+        <div className=" w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-pink-400">02</div>
+        <div className="  w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-pink-400">03</div>
       </div>
 
-      <h1 className='bg-blue-900  text-white my-4 text-center'>Using the container scale</h1>
-      <div className="flex items-center justify-around">
-        <div className="basis-3xs w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-blue-400">01</div>
-        <div className="basis-2xs w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-blue-400">02</div>
-        <div className="basis-xs  w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-blue-400">03</div>
-        <div className="basis-md  w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-blue-400">04</div>
-      </div>
-     
-      <h1 className='bg-blue-900  text-white my-4 text-center'>Using percentages</h1>
-      <div className="flex items-center justify-around">
-        <div className="basis-2/3 w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-green-400">01</div>
-        <div className="basis-1/3 w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-green-400">02</div>
+    
+      <h1 className='bg-blue-900  text-white my-4 text-center'>Row reversed</h1>
+      <div className="flex flex-row-reverse ">
+        <div className=" w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-blue-400">01</div>
+        <div className=" w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-blue-400">02</div>
+        <div className="  w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-blue-400">03</div>
       </div>
 
-      <h1 className='bg-blue-900  text-white my-4 text-center'>Using a custom value</h1>
-      <div className="flex items-center justify-around">
-        <div className="basis-[50vw] w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-yellow-400">01</div>
-        <div className="basis-[200px] w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-yellow-400">02</div> 
-        <div className="basis-(--my-basis) w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-yellow-400">03</div> 
+      <h1 className='bg-blue-900  text-white my-4 text-center'>Column</h1>
+      <div className="flex flex-col items-center gap-2  bg-gray-200 ">
+        <div className=" w-80 h-20 rounded-3xl flex items-center justify-center text-white bg-red-400">01</div>
+        <div className=" w-80 h-20 rounded-3xl flex items-center justify-center text-white bg-red-400">02</div>
+        <div className="  w-80 h-20 rounded-3xl flex items-center justify-center text-white bg-red-400">03</div>
       </div>
     
-
-      <h1 className='bg-blue-900  text-white my-4 text-center'>Customizing your theme</h1>
-      <div className="flex items-center justify-around">
-        <div className="basis-100xs w-30 h-20 rounded-3xl flex items-center justify-center text-white bg-red-400">01</div>
-
+      
+      <h1 className='bg-blue-900  text-white my-4 text-center'>Column reversed</h1>
+      <div className="flex flex-col-reverse items-center gap-2  bg-gray-200 ">
+        <div className=" w-80 h-20 rounded-3xl flex items-center justify-center text-white bg-green-400">01</div>
+        <div className=" w-80 h-20 rounded-3xl flex items-center justify-center text-white bg-green-400">02</div>
+        <div className="  w-80 h-20 rounded-3xl flex items-center justify-center text-white bg-green-400">03</div>
       </div>
       <h1 className='bg-blue-900  text-white my-4 text-center'></h1>
     </>
