ff63b82436160ffe59893cd8e5d7e7df6992b667
amir <amirtaki623@gmail.com>
2025-08-22 20:08:31 +0330

    background-image

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,66 @@ function App() {
   return (
     
     <>    
-      <h1 className='bg-blue-900  text-white my-4 text-center'>background-color</h1>
+      <h1 className='bg-blue-900  text-white my-4 text-center'>background-image</h1>
 
       <h1 className='bg-blue-900  text-white my-4 text-center'>Basic example</h1>
   
-      <div className="flex bg-gray-200 justify-around items-center ">       
-        <button class="bg-blue-500 text-white p-2 rounded-xl">Button A</button>
-        <button class="bg-cyan-500 text-white p-2 rounded-xl">Button B</button>
-        <button class="bg-pink-500 text-white p-2 rounded-xl">Button C</button>
+      <div className="flex h-130 bg-gray-200 justify-around items-center bg-[url(https://cdn.pixabay.com/photo/2016/07/17/19/10/background-1524540_1280.jpg)]">       
+  
       </div>
  
   
-      <h1 className='bg-blue-900  text-white my-4 text-center'>Changing the opacity</h1>
-  
-      <div className="flex bg-gray-200 justify-around items-center ">       
-        <button class="bg-sky-500/100 text-white p-2 rounded-xl">Button A</button>
-        <button class="bg-sky-500/75 text-white p-2 rounded-xl">Button B</button>
-        <button class="bg-sky-500/50 text-white p-2 rounded-xl">Button C</button>
-        <button class="bg-sky-500/25 text-white p-2 rounded-xl">Button D</button>
-      </div>
  
-      <h1 className='bg-blue-900  text-white my-4 text-center'></h1>
-      <h1 className='bg-blue-900  text-white my-4 text-center'></h1>
-      <h1 className='bg-blue-900  text-white my-4 text-center'></h1>
-      <h1 className='bg-blue-900  text-white my-4 text-center'></h1>
+      <h1 className='bg-blue-900  text-white my-4 text-center'>Adding a linear gradient </h1>
+
+      <div className="flex flex-col gap-2 ">
+        <div class="h-14 bg-linear-to-r from-cyan-500 to-blue-500"></div>
+        <div class="h-14 bg-linear-to-t from-sky-500 to-indigo-500"></div>
+        <div class="h-14 bg-linear-to-bl from-violet-500 to-fuchsia-500"></div>
+        <div class="h-14 bg-linear-65 from-purple-500 to-pink-500"></div>```
+      </div>
+
+
+      <h1 className='bg-blue-900  text-white my-4 text-center'>Adding a radial gradient</h1>
+
+      <div className="flex flex-row gap-2 items-center justify-center ">
+        <div class="size-18 rounded-full bg-radial from-pink-400 from-40% to-fuchsia-700"></div>
+        <div class="size-18 rounded-full bg-radial-[at_50%_75%] from-sky-200 via-blue-400 to-indigo-900 to-90%"></div>
+        <div class="size-18 rounded-full bg-radial-[at_25%_25%] from-white to-zinc-900 to-75%"></div>
+
+      </div>
+      <h1 className='bg-blue-900  text-white my-4 text-center'>Adding a conic gradient</h1>
+
+      <div className="flex flex-row gap-2 items-center justify-center ">
+        <div class="size-24 rounded-full bg-conic from-blue-600 to-sky-400 to-50%"></div>
+        <div class="size-24 rounded-full bg-conic-180 from-indigo-600 via-indigo-50 to-indigo-600"></div>
+        <div class="size-24 rounded-full bg-conic/decreasing from-violet-700 via-lime-300 to-violet-700"></div>
+      </div>
+
+
+     
+      <h1 className='bg-blue-900  text-white my-4 text-center'>Setting gradient color stops</h1>
+      
+      <div className="flex flex-row gap-2 items-center justify-center ">
+        <div className="w-full h-20 bg-gradient-to-l from-indigo-500 via-purple-500 to-pink-500"></div>
+      </div>
+      
+      
+      
+      <h1 className='bg-blue-900  text-white my-4 text-center'>Changing interpolation mode</h1>
+      <div className="flex flex-col gap-2 ">
+        <div class="bg-linear-to-r/srgb from-indigo-500 to-teal-400 h-20" ></div>
+        <div class="bg-linear-to-r/hsl from-indigo-500 to-teal-400 h-20" ></div>
+        <div class="bg-linear-to-r/oklab from-indigo-500 to-teal-400 h-20" ></div>
+        <div class="bg-linear-to-r/oklch from-indigo-500 to-teal-400 h-20" ></div>
+        <div class="bg-linear-to-r/longer from-indigo-500 to-teal-400 h-20" ></div>
+        <div class="bg-linear-to-r/shorter from-indigo-500 to-teal-400 h-20" ></div>
+        <div class="bg-linear-to-r/increasing from-indigo-500 to-teal-400 h-20" ></div>
+        <div class="bg-linear-to-r/decreasing from-indigo-500 to-teal-400 h-20" ></div>
+      </div>
+      
+      
+      
       <h1 className='bg-blue-900  text-white my-4 text-center'></h1>
       <h1 className='bg-blue-900  text-white my-4 text-center'></h1>
     
